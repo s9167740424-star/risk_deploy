d6cb343c71d5e3a2a04f9665d19a398940292017
Дарья Герцен <dara.gertsen@Daras-MacBook-Air.local>
2026-07-29 03:38:54 +0300

    parser: fix playwright browser path

--- parser-build.sh
+++ parser-build.sh
@@ -3,4 +3,5 @@ set -o errexit
 cd atlas-sales-backend/rosreestr-parser
 pip install --upgrade pip
 pip install -r requirements-render.txt
+export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/src/.playwright
 playwright install chromium
