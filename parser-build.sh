a8636cdfcdcba11f6c64d9b9ca164b91447973ca
Дарья Герцен <dara.gertsen@Daras-MacBook-Air.local>
2026-07-29 03:36:34 +0300

    parser: playwright without --with-deps

--- parser-build.sh
+++ parser-build.sh
@@ -3,5 +3,4 @@ set -o errexit
 cd atlas-sales-backend/rosreestr-parser
 pip install --upgrade pip
 pip install -r requirements-render.txt
-# ставим только Chromium с системными зависимостями
-playwright install --with-deps chromium
+playwright install chromium
